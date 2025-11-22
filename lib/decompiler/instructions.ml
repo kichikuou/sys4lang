6fb608d755e9178c032468b1ecac2c2312ad4665
kichikuou <KichikuouChrome@gmail.com>
2025-11-22 13:25:35 +0900

    Support X_SET
    As a builtin.

--- lib/decompiler/instructions.ml
+++ lib/decompiler/instructions.ml
@@ -770,6 +770,7 @@ let builtin_method_name = function
   | A_SORT -> "Sort"
   | A_SORT_MEM -> "SortBy"
   | A_REVERSE -> "Reverse"
+  | X_SET -> "Set"
   | DG_CLEAR -> "Clear"
   | DG_EXIST -> "Exist"
   | FTOS -> "String"
